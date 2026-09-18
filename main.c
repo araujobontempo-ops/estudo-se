28190b41b3add2e426edf109d4e8e28a0e6ebd2e
Bruno B Araujo <araujobontempo-ops@github.com>
2026-09-17 23:00:30 -0300

    Unidade 2 - tabuada com while

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define varconst 100
+
+int main () {
+    int x;
+    float c672_7263 = 20.9;
+    x = varconst;
+
+// scanf("%d", &x);
+printf("constant = %d", x);
+    return 0;
+}
---
+++ main3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int res; int x = 1;
+
+    while (x <= 10){ res =2 * x;
+    printf("Result: %d\n", res);
+    x = x + 1;
+
+   }
+   
+    return 0;
+}
